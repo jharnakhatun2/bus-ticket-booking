9ba03ebe80be5e62e07ca4849dffc9f264ee6fd0
jharnakhatun2 <jharnakhatun203@gmail.com>
2024-11-27 08:20:46 -0800

    write javascript code for button click activity

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,40 @@
-console.log("hello")
+// Select elements
+const seatBtn = getAllClass('.seat');
+const totalSeat = getId('totalSeat');
+const seatCount = getId('seatCount');
+const seatInfo = getId('seatInfo');
+
+let buttonCount = 0;
+//Select your seat
+seatBtn.forEach( seat => {
+    seat.addEventListener('click', (event)=>{
+        buttonCount++;
+        const btn = event.target;
+
+        // btn style change
+        btn.style.backgroundColor = '#66CC8A';
+        btn.style.color = '#ffffff';
+
+        //count left seat
+        totalSeat.textContent = 40 - buttonCount ;
+
+        //count total seat
+        seatCount.textContent = buttonCount;
+
+        
+        //set seat info
+        const secondaryDiv = element('div')
+        const div = element('div');
+        div.className = "flex justify-between items-center text-sm"
+        div.innerHTML = `
+          <p>${btn.textContent}</p>
+          <p>Economy</p>
+          <p>550</p>
+        `
+        seatInfo.appendChild(div);
+
+        console.log(btn.textContent);
+        console.log(buttonCount);
+    
+    })
+})
---
+++ src/js/util.js
@@ -0,0 +1,6 @@
+// create element
+const element = tag => document.createElement(tag);
+// get id
+const getId = id => document.getElementById(id);
+// get all class
+const getAllClass = selector => document.querySelectorAll(selector);
